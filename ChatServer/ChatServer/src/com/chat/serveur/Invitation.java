54d802d46a01167771df7c6ab2ca98f36e5b7eef
Hyd0n <159575504+Hyd0n@users.noreply.github.com>
2025-10-20 20:06:40 -0400

    J'envois les classes

---
+++ ChatServer/ChatServer/src/com/chat/serveur/Invitation.java
@@ -0,0 +1,25 @@
+package com.chat.serveur;
+
+import java.util.Objects;
+
+public class Invitation {
+    private String aliasHote;
+    private String aliasInvite; //test
+
+    public Invitation(String aliasHote, String aliasInvite) {
+        this.aliasHote = aliasHote;
+        this.aliasInvite = aliasInvite;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Invitation that = (Invitation) o;
+        return Objects.equals(aliasHote, that.aliasInvite) && Objects.equals(aliasInvite, that.aliasHote);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(aliasHote, aliasInvite);
+    }
+}
