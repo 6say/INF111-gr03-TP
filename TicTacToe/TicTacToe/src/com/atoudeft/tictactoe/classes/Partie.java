1888df1db47dd6bd776cb1ce86b2c78ee3297b80
Corentin <corentin.montreal@hotmail.com>
2025-10-28 17:09:02 -0400

    Il ne me reste plus que la question 4.6 à faire avec MettreAJourStatutApresCoup.

--- TicTacToe/TicTacToe/src/com/atoudeft/tictactoe/classes/Partie.java
+++ TicTacToe/TicTacToe/src/com/atoudeft/tictactoe/classes/Partie.java
@@ -2,6 +2,8 @@ package com.atoudeft.tictactoe.classes;
 
 import com.atoudeft.tictactoe.MethodeNonImplementeeException;
 
+import java.util.List;
+
 public final class Partie {
     private final Plateau plateau = new Plateau();
     private Symbole joueurCourant;
@@ -26,9 +28,25 @@ public final class Partie {
     }
 
     public boolean jouer(Symbole symbole, Position position) {
-        throw new MethodeNonImplementeeException("***** Vous n'avez pas encore implemente la methode : "
-                +Thread.currentThread().getStackTrace()[1].getMethodName()
-                +"() de la classe "+this.getClass().getName());
+
+        if(!isPartieEnCours()){
+            return false; //On s'assure que la partie n'est pas terminée
+        }
+        if(symbole !=joueurCourant){
+            System.out.println("Ce n'est pas à votre tour de jouer"); //J'affiche un message au joueur qui a envoyé une commande quand ce n'est pas son tour
+            return  false; //on retourne false si jamais un coup a été joué par un joueur dont ce n'est pas le tour
+        }
+        Coup coup = new Coup(position, symbole); //On crée un objet coup qui va nous permettre de le mettre sur le plateau
+
+        if(plateau.placer(coup)){ //Si on peut placer le coup, on met à jour le statut
+            mettreAJourStatutApresCoup();
+
+            if( statut == StatutPartie.EN_COURS){ //Si la partie est encore en cours, on change le statut et le symbole du joueur courant
+                joueurCourant = (joueurCourant == Symbole.X) ? Symbole.O : Symbole.X;
+            }
+            return  true; //On return vrai vu que le coup a bien été joué
+        }
+        return  false; //on retourne faux vu que rien n'a pu être joué
     }
     public boolean isPartieEnCours() {
         if (statut != StatutPartie.EN_COURS) {
@@ -37,9 +55,9 @@ public final class Partie {
         return true;
     }
     private void mettreAJourStatutApresCoup() {
-        throw new MethodeNonImplementeeException("***** Vous n'avez pas encore implemente la methode : "
-                +Thread.currentThread().getStackTrace()[1].getMethodName()
-                +"() de la classe "+this.getClass().getName());
+        //On crée un tableau qui va vérifier si regarder si jamais on a une position gagnante
+        List<Position> ligneGagnante = plateau.ligneGagnante();
+     //J'ai pas encore tout fini pour le reste
     }
 
     @Override
