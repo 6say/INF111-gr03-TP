e5e40a1ad9db4ae232727d390d9fb3fc84906af8
Hyd0n <159575504+Hyd0n@users.noreply.github.com>
2025-10-30 18:43:43 -0400

    Modification de la methode mettreAJourStatusApresCoup

--- TicTacToe/TicTacToe/src/com/atoudeft/tictactoe/classes/Partie.java
+++ TicTacToe/TicTacToe/src/com/atoudeft/tictactoe/classes/Partie.java
@@ -2,6 +2,7 @@ package com.atoudeft.tictactoe.classes;
 
 import com.atoudeft.tictactoe.MethodeNonImplementeeException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public final class Partie {
@@ -56,8 +57,20 @@ public final class Partie {
     }
     private void mettreAJourStatutApresCoup() {
         //On crée un tableau qui va vérifier si regarder si jamais on a une position gagnante
-        List<Position> ligneGagnante = plateau.ligneGagnante();
-     //J'ai pas encore tout fini pour le reste
+
+        Symbole gagnant = null;
+        System.out.println(plateau.estPlein());
+        if(plateau.estPlein()){
+            statut = StatutPartie.NULLE;   //Si le plateau est plein alors c'est null
+        }
+        if(!plateau.ligneGagnante().isEmpty()){       //Si la liste gagnante est null rien faire
+           gagnant = plateau.get(plateau.ligneGagnante().get(0).getLigne(), plateau.ligneGagnante().get(0).getColonne() ); //On recupere le symbole du gagnant
+           if(gagnant == Symbole.X){                    //Dependament du Gagnant on change le status
+               statut = StatutPartie.X_GAGNE;
+           }else if(gagnant == Symbole.O){
+               statut = StatutPartie.O_GAGNE;
+           }
+        }
     }
 
     @Override
